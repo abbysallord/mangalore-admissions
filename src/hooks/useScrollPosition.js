844091707db751520448babd56879ec9af4a284f
Dhanush <dshenoyh@gmail.com>
2026-02-28 11:53:36 +0530

    add useScrollPosition hook for navbar scroll detection

---
+++ src/hooks/useScrollPosition.js
@@ -0,0 +1,22 @@
+// src/hooks/useScrollPosition.js
+
+import { useEffect, useState } from 'react';
+
+export default function useScrollPosition() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollY;
+}
